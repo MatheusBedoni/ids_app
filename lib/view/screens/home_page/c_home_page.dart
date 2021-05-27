fcfc748c501af40c720b76527c4ab7a8c47bb3f1
Matheus Bedoni de Sousa <matheus.sousa@sefaz.rr.gov.br>
2021-05-27 14:11:19 -0300

    controller da home page

---
+++ lib/view/screens/home_page/c_home_page.dart
@@ -0,0 +1,12 @@
+import 'package:ids_flutter_app/data/model/people.dart';
+
+class HomeController{
+  List<People> listPeople;
+
+  HomeController({this.listPeople});
+
+
+  void getListOfPeople(){
+
+  }
+}
