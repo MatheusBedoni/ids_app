6c99e6059b7f145464f9decb2d6ffffafe1a9663
matheus bedoni <matheus.sousa@sefaz.rr.gov.br>
2021-05-29 12:50:58 -0400

    adiconar novos parametros

--- lib/view/widgets/w_text_field.dart
+++ lib/view/widgets/w_text_field.dart
@@ -1,23 +1,24 @@
 
 import 'package:flutter/material.dart';
-import 'package:ids_flutter_app/view/screens/cadastro_page/c_register.dart';
 import 'package:ids_flutter_app/view/styles/s_constants.dart';
 
 class TextFieldWidget extends StatelessWidget{
   final TextEditingController value ;
   FormFieldValidator<String> validator;
   String label;
+   IconButton icon;
 
-  TextFieldWidget({this.value,this.label,this.validator});
+
+  TextFieldWidget({this.value,this.label,this.validator,this.icon});
   @override
   Widget build(BuildContext context) {
     return  Container(
       margin:  EdgeInsets.all(15.0),
       padding: EdgeInsets.all(10.0) ,
-      decoration: DecorationConstants.textFieldDecoration,
+      decoration: DecorationConstants.inputDecoration,
       child:TextFormField(
         controller: value,
-        decoration:  InputDecoration(hintText: label,fillColor: Colors.white),
+        decoration:  InputDecoration(hintText: label,fillColor: Colors.white,suffixIcon: icon != null ? icon : null,),
         validator:validator,
       ),
     );
