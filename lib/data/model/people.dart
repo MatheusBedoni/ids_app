6a81975d8096843584b6e0bff40d620e120b835d
matheus bedoni <matheus.sousa@sefaz.rr.gov.br>
2021-05-29 12:47:00 -0400

    metodos para trabalhar com map

--- lib/data/model/people.dart
+++ lib/data/model/people.dart
@@ -3,4 +3,24 @@ class People{
    String nome;
    String dataNascimento;
    String sexo;
+
+   People({this.id,this.nome,this.sexo,this.dataNascimento});
+
+
+   People.fromJson(Map<String, dynamic> json) {
+      id = json['id'];
+      nome = json['nome'];
+      dataNascimento = json['dataNascimento'];
+      sexo = json['sexo'];
+   }
+
+   @override
+   Map<String, dynamic> toMap() {
+      Map<String, dynamic> data = Map();
+      data["id"] = this.id;
+      data["nome"] = this.nome;
+      data["dataNascimento"] =  this.dataNascimento;
+      data["sexo"] =this.sexo;
+      return data;
+   }
 }
