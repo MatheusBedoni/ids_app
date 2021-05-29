de8a2ac3abb6bafbeced092c9b84c386929b8e03
matheus bedoni <matheus.sousa@sefaz.rr.gov.br>
2021-05-29 12:50:04 -0400

    novos parametros

--- lib/view/widgets/w_button.dart
+++ lib/view/widgets/w_button.dart
@@ -3,18 +3,20 @@ import 'package:ids_flutter_app/view/styles/s_constants.dart';
 
 class ButtonWidget extends StatelessWidget{
   String titulo;
+  Color color;
+  Color colorText;
 
-  ButtonWidget({this.titulo});
+  ButtonWidget({this.titulo,this.color,this.colorText});
   @override
   Widget build(BuildContext context) {
     return  Container(
         margin:  EdgeInsets.all(15.0),
-        padding: EdgeInsets.all(10.0) ,
-        decoration:DecorationConstants.buttonDecoration,
+        padding: EdgeInsets.all(20.0) ,
+        decoration:DecorationConstants.getbuttonDecoration(color),
         child:Center(
           child:Text(titulo, style: TextStyle(
             fontWeight: FontWeight.bold,
-            color:  Color(0xFFfff9f9),
+            color: colorText,
             fontSize:  MediaQuery.of(context).size.height/65,
           ),) ,
         )
