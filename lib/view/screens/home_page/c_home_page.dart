90443bd98958d45cffc66b00afaadb8062317650
matheus bedoni <matheus.sousa@sefaz.rr.gov.br>
2021-05-29 12:48:16 -0400

    criacao do metodo lista pessoas

--- lib/view/screens/home_page/c_home_page.dart
+++ lib/view/screens/home_page/c_home_page.dart
@@ -1,12 +1,22 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:ids_flutter_app/data/model/people.dart';
+import 'package:ids_flutter_app/data/shared_database/read_data.dart';
 
 class HomeController{
   List<People> listPeople;
 
+
   HomeController({this.listPeople});
 
 
-  void getListOfPeople(){
-
+  Future<List<People>> getListOfPeople() async {
+      var data = await ReadData.readData('list');
+      if(data != null){
+        final decoded = json.decode(data) as List;
+        return decoded.map((jsonTask) => People.fromJson(jsonTask)).toList();
+      }
+      return [];
   }
 }
